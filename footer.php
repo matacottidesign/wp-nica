3eb43f5e7c68b620732271387158733297eeda85
Francesco Matacotti <francesco1984.go@gmail.com>
2020-06-11 11:01:44 +0200

    aggiunto footer

--- footer.php
+++ footer.php
@@ -21,31 +21,64 @@ $container = get_theme_mod( 'understrap_container_type' );
 	</div>
 </a>
 
-<div class="wrapper" id="wrapper-footer">
-
-	<div class="<?php echo esc_attr( $container ); ?>">
-
-		<div class="row">
-
-			<div class="col-md-12">
-
-				<footer class="site-footer" id="colophon">
-
-					<div class="site-info">
-
-						<?php understrap_site_info(); ?>
-
-					</div><!-- .site-info -->
-
-				</footer><!-- #colophon -->
-
-			</div><!--col end -->
-
-		</div><!-- row end -->
-
-	</div><!-- container end -->
-
-</div><!-- wrapper end -->
+<footer class="bg-secondary mt-1">
+	<div class="container">
+		<div class="row pb-5">
+			<div class="col-6 col-md-4">
+				<!--Orari-->
+				<h4 class="text-primary pt-5">Orari</h4>
+				<ul class="orari-footer">
+					<div class="d-flex justify-content-between">
+						<li>Lun - Ven</li>
+						<li><b>9 - 19</b></li>
+					</div>
+					<div class="d-flex justify-content-between">
+						<li>Sab</li>
+						<li><b>9 - 13</b></li>
+					</div>
+					<div class="d-flex justify-content-between">
+						<li>Urgenze</li>
+						<li><b>h24</b></li>
+					</div>
+				</ul>
+				<!--Dove siamo-->
+				<h4 class="text-primary pt-5">Dove siamo</h4>
+				<ul class="orari-footer">
+					<div class="d-flex justify-content-between">
+						<li><a href="https://www.google.com/maps/place/NI.CA.+S.r.l./@41.7700256,12.659116,15z/data=!4m5!3m4!1s0x0:0xce9c24cdd6f2334e!8m2!3d41.7700256!4d12.659116">Piazza S. Barnaba, 18</a></li>
+						<li>00047 Marino (RM)</li>
+					</div>
+				</ul>
+			</div>
+			<div class="col-6 col-md-4 text-right">
+				<h4 class="text-primary pt-5">Menu</h4>
+				<ul class="orari-footer">
+					<li class="mb-3"><a href="#">Home</a></li>
+					<li class="mb-3"><a href="#">Chi siamo</a></li>
+					<li class="mb-3"><a href="#">Trattamenti</a></li>
+					<li class="mb-3"><a href="#">Blog</a></li>
+					<li><a href="#">Contatti</a></li>
+				</ul>
+			</div>
+			<div class="col-12 col-md-4">
+				<div class="pt-5">
+					<a href="#">
+						<button type="button" class="btn btn-primary w-100">
+							Chiama lo studio
+						</button>
+					</a>
+				</div>
+				<div>
+					<a href="#">
+						<button type="button" class="btn btn-danger mt-3 w-100">
+							Chiama per urgenze
+						</button>
+					</a>
+				</div>
+			</div>
+		</div>
+	</div>
+</footer>
 
 </div><!-- #page we need this extra closing tag here -->
 
